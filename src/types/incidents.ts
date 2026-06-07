6ff03f2a44f371b366528eda8f79277bf6fc05f5
stasyzar <zarovskaanatasia@gmail.com>
2026-06-07 20:55:25 +0300

    refactor(IncidentMarker): make status property optional and clean up formatting

--- src/types/incidents.ts
+++ src/types/incidents.ts
@@ -1,55 +1,34 @@
 export interface IncidentMarker {
-  status: string;
-
   id: string;
-
   lat: number;
   lng: number;
-
   type: 'UAV' | 'Explosion' | 'Siren' | 'Generator';
-
   intensity: number;
-
+  status?: string;
 }
 
 
-
 export interface HistoricalIncident {
-
   id: string;
-
   datetime: string;
-
   type: 'UAV' | 'Siren' | 'Explosion' | 'Generator';
-
   coordinates: string;
-
   confidence: number;
-
   status: 'Resolved' | 'Investigating' | 'Confirmed';
-
 }
 
 
 
 export interface Alert {
-
   id: string;
-
   type: 'UAV' | 'Siren' | 'Explosion' | 'Generator';
-
   timestamp: string;
-
   confidence: number;
-
   location: string;
-
 }
 
 
 
 export interface AlertCardProps {
-
   alert: Alert;
-
 }
